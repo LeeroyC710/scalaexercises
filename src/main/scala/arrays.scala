ff914bedde9e2d53d322fe18b370d7aecb0d304f
leeroyc710 <leeroychiweshe@academytrainee.com>
2020-03-05 16:46:14 +0000

    collections example

---
+++ src/main/scala/arrays.scala
@@ -0,0 +1,14 @@
+object arrays {
+  val array = Array(
+    "Corona",
+    "Stella",
+    "Guinness"
+  )
+  array(0)
+  // Coronaarray.head// Corona
+  // array.last// Guinness
+  // array.tail// Array(Stella, Guinness)
+  // val dimensionalArray = Array.ofDim[Int](2, 2)
+  // dimensionalArray(0)(0) = 1dimensionalArray(0)(1) = 2dimensionalArray(1)(0) = 3dimensionalArray(1)(1) = 4val rangedArray = (1 to 10).toArray[Int]
+  // Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+}

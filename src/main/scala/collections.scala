afc0d7353c3e6bdbcf85afeb0d857f7a4bb33ee7
leeroyc710 <leeroychiweshe@academytrainee.com>
2020-03-05 15:57:23 +0000

    collections example

---
+++ src/main/scala/collections.scala
@@ -0,0 +1,16 @@
+object collections {
+val list: List[String] = List(
+  "Corona",
+  "Stella",
+  "Guinness"
+)
+  println(list(0))
+  println(list.head)
+  println(list.last)
+  println(list.tail)
+
+  val list2 = list :+ "Fosters"
+  // List(Corona, Stella, Guinness, Fosters)val list3 = "Peroni" +: list2
+  // List(Peroni, Corona, Stella, Guinness, Fosters)val combinedList = list ::: list2
+  // List(Corona, Stella, Guinness, Corona, Stella, Guinness, Fosters)val emptyList = List.empty
+}
